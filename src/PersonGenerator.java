ff1db9cd99da11692dd5f046bff2aa9f854263ad
hunter <westerhr@mail.uc.edu>
2025-01-20 10:13:36 -0500

    Initial commit

---
+++ src/PersonGenerator.java
@@ -0,0 +1,11 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PersonGenerator {
+    public static void main(String[] args) {
+        
+    }
+}

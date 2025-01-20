b74b7564d41c38d1d8178b46159d45b9b5c048ec
hunter <westerhr@mail.uc.edu>
2025-01-20 10:33:12 -0500

    Part 1 complete

--- src/PersonGenerator.java
+++ src/PersonGenerator.java
@@ -6,6 +6,35 @@ import java.util.Scanner;
 
 public class PersonGenerator {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        ArrayList<String> personList = new ArrayList();
+        boolean more = true;
+
+        System.out.println("Welcome to the person generator!");
+
+        while(more) {
+            int id = SafeInput.getInt(scan, "Enter ID number");
+            String firstName = SafeInput.getNonZeroLenString(scan, "Enter first name");
+            String lastName = SafeInput.getNonZeroLenString(scan, "Enter last name");
+            String title = SafeInput.getNonZeroLenString(scan, "Enter the title");
+            int birthyear = SafeInput.getRangedInt(scan, "Enter the year of birth", 1000, 2025);
+
+            String record = id + ", " + firstName + ", " + lastName + ", " + title + ", " + birthyear;
+            personList.add(record);
+
+            more = SafeInput.getYNConfirm(scan, "Do you want to add someone else?");
+        }
+
+        String fileName = SafeInput.getNonZeroLenString(scan, "Enter the file name to save the new people to");
+
+        try(FileWriter fileWriter = new FileWriter(fileName)) {
+            for(String person : personList) {
+                fileWriter.write(person + "\n");
+            }
+            System.out.println("Data saved successfully to " + fileName);
+        } catch(IOException e) {
+            System.out.println("An error occurred while writing to the file.");
+            e.printStackTrace();
+        }
     }
 }
--- src/PersonReader.java
+++ src/PersonReader.java
@@ -1,2 +1,12 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import java.util.Scanner;
+
+import javax.swing.JFileChooser;
+
 public class PersonReader {
+    public static void main(String[] args) {
+
+    }
 }
